b75e8e7173f43dc335286f4e1f585c7d9e70f26c
yalexey-jcode <yalen316@gmail.com>
2026-01-17 22:41:17 +0300

    Add files via upload

--- include/xsimd/config/xsimd_arch.hpp
+++ include/xsimd/config/xsimd_arch.hpp
@@ -171,7 +171,7 @@ namespace xsimd
     using all_arm_architectures = typename detail::join<all_sve_architectures, arch_list<i8mm<neon64>, neon64, neon>>::type;
     using all_power_architectures = arch_list<vsx>;
     using all_riscv_architectures = all_rvv_architectures;
-    using all_wasm_architectures = arch_list<wasm>;
+    using all_wasm_architectures = arch_list<wasm_relaxed, wasm>;
     using all_architectures = typename detail::join<all_power_architectures, all_riscv_architectures, all_wasm_architectures, all_arm_architectures, all_x86_architectures>::type;
 
     using supported_architectures = typename detail::supported<all_architectures>::type;
